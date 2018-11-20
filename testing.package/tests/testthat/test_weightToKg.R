a51c4b6990f90610e10364b8b5e5c08a895c4382
Mateusz Cytrowski <mateusz.cytrowski@lingarogroup.com>
2018-11-20 13:51:42 +0100

    improved testthat for the package

--- testing.package/tests/testthat/test_weightToKg.R
+++ testing.package/tests/testthat/test_weightToKg.R
@@ -8,6 +8,9 @@ test_that("function is returning data frame", {
 })
 
 test_that("output has right format", {
+  df_test_3 = weightToKg(mtcars)
   expect_equal(ncol(df_test_3), 2)
   expect_equal(nrow(df_test_3), nrow(mtcars))
+  expect_is(df_test_3$weight_kg, "numeric")
+  expect_true(all(df_test_3$weight_kg < 3000))
 })
