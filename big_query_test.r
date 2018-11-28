6f1d07d38b369635c22527fd4e593d09c9c905d7
Mateusz Cytrowski <mateusz.cytrowski@lingarogroup.com>
2018-11-28 13:26:36 +0100

    running connection and functionality test on CGP bigquery and r

--- big_query_test.r
+++ big_query_test.r
@@ -1,2 +1,33 @@
+# Following post from:
+# https://cloud.google.com/blog/products/gcp/google-cloud-platform-for-data-scientists-using-r-with-google-bigquery
+
+
 library(bigrquery)
+
+### Connect to table
+
+# Select project
+project = "trim-tide-223909"
+
+# select query
+sql = "SELECT * FROM campaigData.mtcars"
+
+# download dataset and authenticate the account
+df_query = query_exec(sql, project = project, useLegacySql = FALSE)
+
+
+# Select the count of cars with mpg higher than 16
+sql = "SELECT COUNT(mpg) FROM campaigData.mtcars
+WHERE mpg > 16"
+q_mpg16 = query_exec(sql, project = project, useLegacySql = FALSE)
+
+
+# Ratio of mpg > 16 as pie chart
+sql = "SELECT COUNT(mpg) FROM campaigData.mtcars"
+q_mpgall = query_exec(sql, project = project, useLegacySql = FALSE)
+
+p_mpg = c(q_mpg16[1,1], q_mpgall[1,1] - q_mpg16[1,1])
+labels_mpg = c("<16mpg", ">16mpg")
+pie(p_mpg, labels = labels_mpg)
+
 
